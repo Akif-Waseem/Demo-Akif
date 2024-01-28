bdd641e31fc7f3ca3fdba1afa593f30fa634c1c6
Akif-Waseem <157923622+Akif-Waseem@users.noreply.github.com>
2024-01-27 23:10:53 -0500

    Updated Org repository methods

--- src/main/java/com/freddiemac/poc/demo/repository/OrgRepository.java
+++ src/main/java/com/freddiemac/poc/demo/repository/OrgRepository.java
@@ -7,5 +7,19 @@ import com.freddiemac.poc.demo.domain.OrgDomain;
 
 @Repository
 public interface OrgRepository extends JpaRepository<OrgDomain, Integer>{
+	
+	// craete method to get org details of largest org id.
+	OrgDomain findTopByOrderByOrgIdDesc();
+	
+	// craete method to get org details if account domain is present.
+	OrgDomain findByAccountDomain_AccountId(Integer accountId);
+	
+	// craete method to get org details if address domain is present.
+	OrgDomain findByAddressDomain_AddressId(Integer addressId);
+	
+	// craete method to get Address domain by org id.
+	OrgDomain findByAddressDomain_OrgId(Integer orgId);
+
+	
 
 }
